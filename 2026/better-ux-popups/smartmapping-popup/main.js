dc9c3e77f9079f42a24196ce3565f1694edad29d
Lauren Boyd <lboyd@esri.com>
2026-03-08 21:02:18 -0700

    chore: rm field formatting

--- 2026/better-ux-popups/smartmapping-popup/main.js
+++ 2026/better-ux-popups/smartmapping-popup/main.js
@@ -1,11 +1,13 @@
-  (async () => {
+(async () => {
   const control = document.getElementById("control");
   // Get a reference to the arcgis-map component
   const arcgisMap = document.getElementById("my-map");
-  const popupTemplateCreator = await $arcgis.import("@arcgis/core/smartMapping/popup/templates.js")
+  const popupTemplateCreator = await $arcgis.import(
+    "@arcgis/core/smartMapping/popup/templates.js",
+  );
 
   await arcgisMap.viewOnReady();
-  
+
   arcgisMap.popupElement.dockEnabled = true;
   arcgisMap.popupElement.dockOptions = {
     breakpoint: false,
@@ -20,15 +22,6 @@
   const layer = arcgisMap.map.findLayerById("18db3b41795-layer-3");
   await layer.load();
 
-  // Remove fieldConfiguration & fieldInfo formatting from the layer so we can configure these manually.
-  layer.fieldConfigurations = null;
-  layer.popupTemplate.content.fieldInfos.forEach((fieldInfo) => {
-    fieldInfo.format = null;
-    fieldInfo.fieldFormat = null;
-  });
-
-
-  // TODO: add code to format the popup with FieldConfigurations
 
   const response = await popupTemplateCreator.getTemplates({
     layer: layer,
